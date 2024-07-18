8faa06b57771112bc88b6675351a54ccdbae1471
Nitiphumi Dibprakhon <nitiphumidibprakhon@gmail.com>
2024-07-18 14:28:31 +0700

    version1.2

---
+++ Button_1.java
@@ -0,0 +1,43 @@
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
+import javax.swing.plaf.basic.BasicButtonUI;
+
+import java.awt.*;
+import javax.swing.*;
+
+public class Button_1 extends JButton{
+    public Button_1(String text) {
+        super(text);
+        setContentAreaFilled(false);
+        setFocusPainted(false);
+        setBorder(BorderFactory.createEmptyBorder(21, 85,21, 85));
+        setBackground(new Color(254,169,169));
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        Graphics2D g2 = (Graphics2D) g.create();
+        
+        
+        // วาดปุ่ม
+        if (getModel().isArmed()) {
+            g2.setColor(new Color(255,192,203));
+        } else {
+            g2.setColor(getBackground());
+        }
+        g2.fillRoundRect(0, 0, getWidth() - 1, getHeight() - 1, 30, 30);
+        
+        //วาดขอบ
+        g2.setColor(Color.BLACK);
+        g2.drawRoundRect(0, 0, getWidth() - 1, getHeight() - 1, 30, 30);
+
+        g2.dispose();
+        
+        super.paintComponent(g);
+    }
+
+     @Override
+    public void updateUI() {
+        setUI(new BasicButtonUI());
+    }
+}
--- menu.java
+++ menu.java
@@ -1,13 +1,53 @@
 import java.awt.*;
+import java.util.concurrent.Flow;
+
 import javax.swing.*;
-public class menu extends JFrame{
-        public static void main(String[] args) {
-            JFrame frame = new JFrame("GUI");
-            frame.setLayout(null);
-            frame.setBounds(450, 300, 1280, 720);
-            JButton button = new JButton();
-            frame.add(button);
-            frame.setVisible(true);
-            frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        }
+public class menu{
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("GUI");
+        frame.setLayout(new BorderLayout());
+        frame.setBounds(450, 300, 1280, 720);
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
+        Font font = new Font("Tahoma",Font.BOLD,100);
+        Font font1 = new Font("Tahoma",Font.BOLD,25);
+
+        JPanel panel = new JPanel();
+        panel.setLayout(new GridBagLayout()); // ใช้ GridBagLayout
+        GridBagConstraints gbc = new GridBagConstraints();
+        Label label = new Label("PM2.5");
+        label.setFont(font);
+        gbc.gridy = 0; 
+        panel.add(label,gbc);
+        gbc.fill = GridBagConstraints.HORIZONTAL;
+        gbc.insets = new Insets(10, 0, 10, 0); // ช่องว่างระหว่างปุ่ม
+
+        // สร้างปุ่มและเพิ่มลงใน JPanel
+        Button_1 button0 = new Button_1("Start");
+        button0.setPreferredSize(new Dimension(300, 75));
+        gbc.gridx = 0;
+        gbc.gridy = 1;
+        button0.setFont(font1);
+        panel.add(button0, gbc);
+
+        Button_1 button1 = new Button_1("Organizer");
+        button1.setPreferredSize(new Dimension(300, 75));
+        gbc.gridx = 0;
+        gbc.gridy = 2;
+        button1.setFont(font1);
+        panel.add(button1, gbc);
+
+        Button_1 button2 = new Button_1("Exit");
+        button2.setPreferredSize(new Dimension(300, 75));
+        gbc.gridx = 0;
+        gbc.gridy = 3;
+        button2.setFont(font1);
+        panel.add(button2, gbc);
+
+        panel.setFont(font);
+        // เพิ่ม panel ลงใน frame
+        frame.add(panel, BorderLayout.CENTER);
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+    }
 }
