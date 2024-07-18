d8e11d48eaade60b2b1e62df77adeaba014c5fd4
Nitiphumi Dibprakhon <nitiphumidibprakhon@gmail.com>
2024-07-18 19:36:09 +0700

    version1.3

--- menu.java
+++ menu.java
@@ -1,17 +1,23 @@
-import java.awt.*;
 import java.util.concurrent.Flow;
-
-import javax.swing.*;
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
+import java.awt.Label;
+import javax.swing.*;// import is for folder
 public class menu{
     public static void main(String[] args) {
         JFrame frame = new JFrame("GUI");
         frame.setLayout(new BorderLayout());
-        frame.setBounds(450, 300, 1280, 720);
+        frame.setBounds(0, 0, 1280, 720);
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-
+        
+        Dimension size = new Dimension(300, 75);
         Font font = new Font("Tahoma",Font.BOLD,100);
         Font font1 = new Font("Tahoma",Font.BOLD,25);
-
+        
         JPanel panel = new JPanel();
         panel.setLayout(new GridBagLayout()); // ใช้ GridBagLayout
         GridBagConstraints gbc = new GridBagConstraints();
@@ -21,29 +27,29 @@ public class menu{
         panel.add(label,gbc);
         gbc.fill = GridBagConstraints.HORIZONTAL;
         gbc.insets = new Insets(10, 0, 10, 0); // ช่องว่างระหว่างปุ่ม
-
+        
         // สร้างปุ่มและเพิ่มลงใน JPanel
         Button_1 button0 = new Button_1("Start");
-        button0.setPreferredSize(new Dimension(300, 75));
+        button0.setPreferredSize(size);
         gbc.gridx = 0;
         gbc.gridy = 1;
         button0.setFont(font1);
         panel.add(button0, gbc);
-
+        
         Button_1 button1 = new Button_1("Organizer");
-        button1.setPreferredSize(new Dimension(300, 75));
+        button1.setPreferredSize(size);
         gbc.gridx = 0;
         gbc.gridy = 2;
         button1.setFont(font1);
         panel.add(button1, gbc);
-
+        
         Button_1 button2 = new Button_1("Exit");
-        button2.setPreferredSize(new Dimension(300, 75));
+        button2.setPreferredSize(size);
         gbc.gridx = 0;
         gbc.gridy = 3;
         button2.setFont(font1);
         panel.add(button2, gbc);
-
+        
         panel.setFont(font);
         // เพิ่ม panel ลงใน frame
         frame.add(panel, BorderLayout.CENTER);
--- organizer.java
+++ organizer.java
@@ -1,3 +1,39 @@
+import java.awt.*;
+import javax.swing.*;
+
 class organizer {
-    
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("GUI");
+        frame.setLayout(new BorderLayout());
+        frame.setBounds(450, 300, 1280, 720);
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        
+        JPanel panel0 = new JPanel();
+        JPanel panel1 = new JPanel();
+        JPanel panel2 = new JPanel();
+        JPanel panel3 = new JPanel();
+        panel0.setLayout(new GridBagLayout());
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbc.gridx = 0;
+        panel1.setLayout(new GridLayout());
+        panel2.setLayout(new GridLayout());
+        panel3.setLayout(new GridLayout());
+        Button_1 button = new Button_1("Test");
+        button.setSize(200,500);
+        Button_1 button1 = new Button_1("Test");
+        button.setSize(200,500);
+        Button_1 button2 = new Button_1("Test");
+        button.setSize(200,500);
+        button1.setSize(200, 500);
+        panel1.add(button);
+        panel2.add(button1);
+        panel3.add(button2);
+        panel0.add(panel1,gbc);
+        gbc.gridx = 1;
+        panel0.add(panel2,gbc);
+        gbc.gridx = 2;
+        panel0.add(panel3,gbc);
+        frame.add(panel0);
+        frame.setVisible(true);
+    }
 }
