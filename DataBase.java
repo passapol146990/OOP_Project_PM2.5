7cc386a7fbef966a4d8c296215cd04066a9cab7d
Passapol <phasphlsuthahthrrm976@gmail.com>
2024-08-02 17:08:27 +0700

    Update DataBase.java

--- DataBase.java
+++ DataBase.java
@@ -4,29 +4,38 @@ import java.io.File;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+
 public class DataBase {
+    private int peoplemin=5000;
+    private int peoplemax=5000;
+    DataBase(){
+        try{
+            Scanner readFile = new Scanner(new File("./database/data.txt"));
+            while (readFile.hasNext()) {
+                String dataString = readFile.nextLine();
+                Pattern p = Pattern.compile("peoplemin=(\\d+),peoplemax=(\\d+)");
+                Matcher matcher = p.matcher(dataString);
+                if (matcher.find()) {
+                    this.peoplemin = Integer.parseInt(matcher.group(1));
+                    this.peoplemax = Integer.parseInt(matcher.group(2));
+                }
+            }
+        }catch(Exception e){System.out.println(e);}
+    }
     public static void main(String[] args){
-        // try{
-        //     Scanner readFile = new Scanner(new File("./database/data.txt"));
-        //     while (readFile.hasNext()) {
-        //         for(String i:readFile.nextLine().split(",")){
-        //             System.out.println(i);
-        //         }
-        //     }
-        // }catch(Exception e){System.out.println(e);}
-        Pattern p = Pattern.compile("peoplemin=(.*?),");
-        Matcher m = p.matcher("peoplemin=0,peoplemax=100");
-        System.out.println(m.matches());
-        System.out.println(p.compile("peoplemin=0,peoplemax=100"));
-
-
+        DataBase data = new DataBase();
+        System.out.println(data.getMaxPeople()+"\n"+data.getMinPeople());
     }
     int getMinPeople(){
-        return 0;
+        return this.peoplemin;
     }
     int getMaxPeople(){
-        return 0;
+        return this.peoplemax;
     }
-    void setMinPeople(int x){}
-    void setMaxPeople(int x){}
+    void setMinPeople(int x){
+        this.peoplemin = x;
+    }
+    void setMaxPeople(int x){
+        this.peoplemax = x;
+    }
 }
