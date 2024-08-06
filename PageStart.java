34993aaef451feb6b6f5377eaae330d4b07d89b6
Passapol <phasphlsuthahthrrm976@gmail.com>
2024-08-07 02:24:01 +0700

    Update PageStart.java

--- PageStart.java
+++ PageStart.java
@@ -89,44 +89,7 @@ class ShowStatusArea extends JPanel{
         setBounds(1000, 50, 250, 500);
         setBorder(new LineBorder(Color.BLACK));
     }
-    void clickAreas(){
-
-        JPanel pic_of_feeling = new JPanel();
-        pic_of_feeling.setLayout(new FlowLayout());
-        pic_of_feeling.setBounds(1000,50 , 250, 250);
-        ImageIcon icon_happy = new ImageIcon("./image/happy.png");
-        JLabel label_happy = new JLabel(new ImageIcon(icon_happy.getImage().getScaledInstance(240,240,icon_happy.getImage().SCALE_SMOOTH)));
-        ImageIcon icon_sad = new ImageIcon("./image/sad.png");
-        JLabel label_sad = new JLabel(new ImageIcon(icon_sad.getImage().getScaledInstance(240,240,icon_sad.getImage().SCALE_SMOOTH)));
-        ImageIcon icon_sick = new ImageIcon("./image/sick.png");
-        JLabel label_sick = new JLabel(new ImageIcon(icon_sick.getImage().getScaledInstance(240,240,icon_sick.getImage().SCALE_SMOOTH)));
-        ImageIcon icon_normal = new ImageIcon("./image/normal.png");
-        JLabel label_normal = new JLabel(new ImageIcon(icon_normal.getImage().getScaledInstance(240,240,icon_normal.getImage().SCALE_SMOOTH)));
-        Border O = BorderFactory.createLineBorder(Color.BLACK,2);
-        Border I = BorderFactory.createEmptyBorder(-2,0,0,0);
-        pic_of_feeling.setBorder(BorderFactory.createCompoundBorder(O, I));
-        pic_of_feeling.add(label_sick);
-        add(pic_of_feeling);
-        
-        // importข้อความ
-        JPanel panel_text_feeling = new JPanel();
-        Font font_text= new Font("Tahoma",Font.BOLD,20);
-        panel_text_feeling.setBounds(1000,301 , 250, 350);
-        panel_text_feeling.setLayout(new FlowLayout());
-        panel_text_feeling.setBorder(BorderFactory.createCompoundBorder(O, I));
-        JLabel label_text_feeling = new JLabel();
-        label_text_feeling.setFont(font_text);
-        label_text_feeling.setText("<html><div style='text-align: left;'>ปริมาณฝุ่น "+ 5 + "%<br>ประชากกรทั้งหมด"+100+" คน<br>ประชากรที่สุขภาพดี "+99+" คน<br>ประชากรที่ป่วย "+1+" คน<br>ร้อยละคนป่วย "+1+"%</div></html>");
-        label_text_feeling.setVerticalAlignment(JLabel.CENTER);
-        label_text_feeling.setHorizontalAlignment(JLabel.LEFT);
-        panel_text_feeling.add(label_text_feeling);
-        add(panel_text_feeling);
-        
-       
-        
-    }
-    void set_status(float persen)
-    {
+    void set_status(float persen){
         JPanel pic_of_feeling = new JPanel();
         pic_of_feeling.setLayout(new FlowLayout());
         pic_of_feeling.setBounds(1000,50 , 250, 250);
@@ -140,10 +103,6 @@ class ShowStatusArea extends JPanel{
         JLabel label_normal = new JLabel(new ImageIcon(icon_normal.getImage().getScaledInstance(200,200,icon_normal.getImage().SCALE_SMOOTH)));
         Border O = BorderFactory.createLineBorder(Color.BLACK,2);
         Border I = BorderFactory.createEmptyBorder(-2,0,0,0);
-       
-        
-        
-        
         // importข้อความ
         JPanel panel_text_feeling = new JPanel();
         Font font_text= new Font("Tahoma",Font.BOLD,20);
@@ -158,24 +117,19 @@ class ShowStatusArea extends JPanel{
         label_text_feeling.setHorizontalAlignment(JLabel.LEFT);
         panel_text_feeling.add(label_text_feeling);
         panel_text_feeling.setBackground(getBackground());
-        
         removeAll();
-        if (persen >= 30 ) {
-           
+        if (persen >= 30 ){
             pic_of_feeling.add(label_sick);
         }
-        else if (persen >= 20 ) {
-           
+        else if (persen >= 20){
             pic_of_feeling.add(label_sad);
         }
-        else if (persen >=10){
+        else if (persen >= 10){
             pic_of_feeling.add(label_normal);
         }
-        else if (persen >=0){
-           
+        else if (persen >= 0){
             pic_of_feeling.add(label_happy);
         }
-       
         add(pic_of_feeling);
         add(panel_text_feeling);
         revalidate();
@@ -225,41 +179,6 @@ class InputPeople extends JPanel{
 }
 
 class About_Methods {
-    void setDatas(float[][] datas, JPanel panelDatas){
-        About_Methods methods = new About_Methods();
-        for(int i=0;i<datas.length;i++){
-            JPanel rowDatas = new JPanel();
-            rowDatas.setLayout(new GridLayout());
-            for(int j=0;j<datas[i].length;j++){
-                JButton button = new JButton();
-                button.setBounds(0,0,50,50);
-                button.setBackground(methods.getColor(methods.CaladerPerSen((float)datas[i][j])));
-                button.putClientProperty("row",i);
-                button.putClientProperty("col",j);
-                button.putClientProperty("persen",methods.CaladerPerSen((float)datas[i][j]));
-                button.addActionListener(new ActionListener() {
-                    public void actionPerformed(ActionEvent e) {
-                        JButton sourceButton = (JButton) e.getSource();
-                        int row = (int) sourceButton.getClientProperty("row");
-                        int col = (int) sourceButton.getClientProperty("col");
-                        int persen = (int) sourceButton.getClientProperty("persen");
-                        // System.out.println(datas.get(row).get(col));
-                        System.out.println(persen);
-                        // methods.add_score(datas,row,col);
-                        
-                    }
-                });
-                // JButton clearButton = new JButton("Clear Panel");
-                // clearButton.addActionListener(e -> {
-                //     panelDatas.removeAll();  // Remove all components from the panel
-                //     panelDatas.revalidate();  // Revalidate the panel to refresh the layout
-                //     panelDatas.repaint();     // Repaint the panel to update the UI
-                // });
-                rowDatas.add(button);
-            }
-            panelDatas.add(rowDatas);
-        }
-    }
     Color getColor(float persen){
         if(persen>30){
             return new Color(178,0,0);
@@ -291,90 +210,4 @@ class About_Methods {
         }  
         return persen;
     }
-    void add_score(float[][] datas, int x, int y){
-        int rows = datas.length;
-        int cols = datas[0].length;
-        int resultSize = 3; // Size of the result array
-        float[][] result = new float[resultSize][resultSize];
-    
-        // Initialize result array with -1 or any placeholder
-        for (int i = 0; i < resultSize; i++) {
-            Arrays.fill(result[i], -1); // Use -1 to indicate empty values
-        }
-    
-        // Fill the result array
-        int resultRow = 0;
-        // on data
-        if (x - 1 >= 0) {
-            int resultCol = 0;
-            if (y - 1 >= 0) result[resultRow][resultCol++] = datas[x - 1][y - 1];
-            result[resultRow][resultCol++] = datas[x - 1][y];
-            if (y + 1 < cols) result[resultRow][resultCol] = datas[x - 1][y + 1];
-            resultRow++;
-        }
-    
-        // center data
-        int centerRow = resultRow;
-        int centerCol = 0;
-        if (y - 1 >= 0) result[centerRow][centerCol++] = datas[x][y - 1];
-        result[centerRow][centerCol++] = datas[x][y];
-        if (y + 1 < cols) result[centerRow][centerCol] = datas[x][y + 1];
-        resultRow++;
-    
-        // under data
-        if (x + 1 < rows) {
-            int underRow = resultRow;
-            int underCol = 0;
-            if (y - 1 >= 0) result[underRow][underCol++] = datas[x + 1][y - 1];
-            result[underRow][underCol++] = datas[x + 1][y];
-            if (y + 1 < cols) result[underRow][underCol] = datas[x + 1][y + 1];
-        }
-    
-        // Print the result array, skipping -1 values
-        for (float[] row : result) {
-            boolean hasNonEmptyValue = false;
-            StringBuilder sb = new StringBuilder();
-            sb.append("[");
-            for (float value : row) {
-                if (value != -1) {
-                    sb.append(value).append(" ");
-                    hasNonEmptyValue = true;
-                }
-            }
-            sb.append("]");
-            if(hasNonEmptyValue)
-            {
-                System.out.println(sb.toString());
-            }
-        }
-    }
-    // void rainRoyal(ArrayList<ArrayList<Float>>datas,int x,int y){
-    //     if (y-1>=0) {
-    //             datas.get(x).set(y - 1, (float)datas.get(x).get(y - 1) - 1);
-    //         }
-    //         datas.get(x).set(y, (float)datas.get(x).get(y) - 1);
-    //         if (y+1<=19) {
-    //             datas.get(x).set(y + 1, (float)datas.get(x).get(y + 1) - 1);
-    //         }
-    //         // 
-    //         if (x-1>=0) {
-    //             if (y-1>=0) {
-    //                 datas.get(x - 1).set(y - 1, (float)datas.get(x-1).get(y - 1) - 1);
-    //             }
-    //             datas.get(x-1).set(y, (float)datas.get(x-1).get(y) - 1);
-    //             if (y+1<=19) {
-    //                 datas.get(x-1).set(y + 1, (float)datas.get(x-1).get(y + 1) - 1);
-    //             }
-    //         }
-    //         // 
-    //         if(x+1<=19) {
-    //             if (y-1>=0) {
-    //                 datas.get(x+1).set(y - 1, (float)datas.get(x+1).get(y - 1) - 1);
-    //             }
-    //             datas.get(x+1).set(y, (float)datas.get(x+1).get(y) - 1);
-    //             if (y+1<=19) {
-    //                 datas.get(x+1).set(y + 1, (float)datas.get(x+1).get(y + 1) - 1);
-    //             }
-    //         }
-    // }
 }
