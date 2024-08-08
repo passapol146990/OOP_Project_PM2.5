9e2a94ad84131f039abc0113cd5fcdaa56b9141f
POOMIX <92581505+POOMIX@users.noreply.github.com>
2024-08-08 15:13:14 +0700

    Up-Status-nothing
    เพิ่มหน้าnothing

--- PageStart.java
+++ PageStart.java
@@ -133,6 +133,9 @@ class ShowStatusArea extends JPanel{
             JLabel label_happy = new JLabel(new ImageIcon(icon_happy.getImage().getScaledInstance(200,200,icon_happy.getImage().SCALE_SMOOTH)));
             pic_of_feeling.add(label_happy);
         }else{
+            ImageIcon icon_nothing = new ImageIcon("./image/nothing.png");
+            JLabel label_nothing= new JLabel(new ImageIcon(icon_nothing.getImage().getScaledInstance(200,200,icon_happy.getImage().SCALE_SMOOTH)));
+            pic_of_feeling.add(label_nothing);
             label_text_feeling.setText("<html><div style='text-align: left;'>ปริมาณฝุ่น "+0 + "<br>ประชากกรทั้งหมด"+0+" คน<br>ประชากรที่สุขภาพดี "+0+" คน<br>ประชากรที่ป่วย "+0+" คน<br>ร้อยละคนป่วย "+0+"%</div></html>");
         }
         panel_text_feeling.add(label_text_feeling);
